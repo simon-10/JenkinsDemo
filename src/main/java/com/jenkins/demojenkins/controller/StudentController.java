c722c86330cd0ef85ff351f3b6bb036fe0b7dded
simon-10 <65138110+simon-10@users.noreply.github.com>
2020-10-07 17:35:23 -0500

    added by simon

--- src/main/java/com/jenkins/demojenkins/controller/StudentController.java
+++ src/main/java/com/jenkins/demojenkins/controller/StudentController.java
@@ -1,9 +1,14 @@
 package com.jenkins.demojenkins.controller;
 
+import com.jenkins.demojenkins.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
 
 
 
