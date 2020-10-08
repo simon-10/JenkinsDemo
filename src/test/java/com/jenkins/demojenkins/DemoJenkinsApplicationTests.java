fe3316b9af3fea6d2be2e959276fb2cab89612c6
simon-10 <65138110+simon-10@users.noreply.github.com>
2020-10-07 19:09:18 -0500

    one test and yaml added

--- src/test/java/com/jenkins/demojenkins/DemoJenkinsApplicationTests.java
+++ src/test/java/com/jenkins/demojenkins/DemoJenkinsApplicationTests.java
@@ -1,10 +1,40 @@
 package com.jenkins.demojenkins;
 
+import com.jenkins.demojenkins.domain.User;
+import com.jenkins.demojenkins.repository.UserRepository;
+import com.jenkins.demojenkins.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class DemoJenkinsApplicationTests {
+
+	@Autowired
+	private UserService userService;
+
+	@MockBean
+	private UserRepository userRepository;
+
+	@Test
+	public void getUsersTest(){
+		when(userRepository.findAll()).thenReturn(Arrays.asList(
+				new User(9,"TestName",9, "TestAddress1"),
+				new User(99,"TestName",99, "TestAddress2")
+		));
+
+		assertEquals(2,userService.getUsers().size());
+	}
+
+
+
+
 
 	@Test
 	void contextLoads() {
