52bcbaf20a0996dda63b2214d3c11400b305bb48
simon-10 <65138110+simon-10@users.noreply.github.com>
2020-10-08 15:53:11 -0500

    completed test class

--- src/test/java/com/jenkins/demojenkins/DemoJenkinsApplicationTests.java
+++ src/test/java/com/jenkins/demojenkins/DemoJenkinsApplicationTests.java
@@ -11,7 +11,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class DemoJenkinsApplicationTests {
@@ -37,7 +37,29 @@ class DemoJenkinsApplicationTests {
 
 
 	@Test
-	void contextLoads() {
+	public void getUserByAddressTest() {
+		String address="Mendefera";
+		when(userRepository.findByAddress(address)).thenReturn(Arrays.asList(
+				new User(99,"TestName",99, "TestMendefera"))
+		);
+
+		assertEquals(1, userService.getUserByAddress(address).size());
+
+	}
+
+	@Test
+	public void addUserTest(){
+		User user=new User(999, "TestName", 999, "TestAddressAdd");
+		when(userRepository.save(user)).thenReturn(user);
+		assertEquals(user,userService.addUser(user));
+	}
+
+	@Test
+	public void deleteUserTest(){
+		User user=new User(9999, "TestName", 9999, "TestAddressDelete");
+		userService.deleteUser(user);
+		verify(userRepository, times(1)).delete(user);
+		//verify(userRepository,times(2)).delete(user);
 	}
 
 }
