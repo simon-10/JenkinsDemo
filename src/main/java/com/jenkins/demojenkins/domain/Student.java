7d4abcbfcf10eb59265b7787694957a530392c79
simon-10 <65138110+simon-10@users.noreply.github.com>
2020-11-02 11:57:57 -0600

    student repository completed

--- src/main/java/com/jenkins/demojenkins/domain/Student.java
+++ src/main/java/com/jenkins/demojenkins/domain/Student.java
@@ -2,18 +2,18 @@ package com.jenkins.demojenkins.domain;
 
 
 public class Student {
-    private int id;
+    private Integer id;
     private String name;
     private String address;
 
 
-    public Student(int id, String name, String address) {
+    public Student(Integer id, String name, String address) {
         this.id = id;
         this.name = name;
         this.address = address;
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
--- src/main/java/com/jenkins/demojenkins/repository/StudentRepository.java
+++ src/main/java/com/jenkins/demojenkins/repository/StudentRepository.java
@@ -1,4 +1,7 @@
 package com.jenkins.demojenkins.repository;
 
-public class StudentRepository {
+import com.jenkins.demojenkins.domain.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
 }
